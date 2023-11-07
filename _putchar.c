227c6f8515225000e3d87684f32261eb9c360c5c
Wenzile-Vezi <wenzile.plenty@gmail.com>
2023-11-07 10:39:06 -0800

    Files

---
+++ _putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout.
+ * @c: the character to print
+ *
+ * Return: 1 on success.
+ * on error -1 is returned and erno is set appropriately
+ */
+int _putchar(char c)
+{
+	static int a;
+	static char buff[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE);
+	{
+		write(1, buff, a);
+		a = 0;
+	}
+	if (c != BUF_FLUSH)
+		buff[++a] = c;
+	return (a);
+}
+
+	

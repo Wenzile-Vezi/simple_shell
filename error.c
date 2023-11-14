2aa3928f87580d592b9b191b0ebd4925b52f815e
Wenzile-Vezi <wenzile.plenty@gmail.com>
2023-11-14 01:32:03 -0800

    almost done

---
+++ error.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - writes the character c to stderr
+ * @c: the character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and ernno is set appropriately
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[I++] = c;
+	return (1);
+}
+/**
+ * _putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	whle (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
